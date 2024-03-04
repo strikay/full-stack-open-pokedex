b5413954ca81910de7a9dbc3f3ed4593d388eeab
strikay <strikay8@gmail.com>
2024-03-04 13:08:13 +0200

    Simulating a broken healthcheck endpoint

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.static('dist'))
 
 //the health check endpoint of the app
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
